6642e3323f628d4aa7b186b8c6015e3ec4317f89
Yuxuan Zhang <zhangy6x@gmail.com>
2018-02-13 13:52:07 -0500

    updated

--- compute.py
+++ compute.py
@@ -14,15 +14,20 @@ def compute(A, b, w, T, resolution=500):
     plt.figure()  # needed to avoid adding curves in plot
     plt.plot(t, u)
     plt.title('A=%g, b=%g, w=%g' % (A, b, w))
-    if not os.path.isdir('static'):
-        os.mkdir('static')
+    abspath = os.path.abspath(os.path.dirname(__file__))
+    static_dir = os.path.join(abspath, 'static')
+    temp_file_dir = os.path.join(static_dir, 'temp_file')
+    if not os.path.isdir(static_dir):
+        os.mkdir(static_dir)
+    if not os.path.isdir(temp_file_dir):
+        os.mkdir(temp_file_dir)
     else:
         # Remove old plot files
-        for filename in glob.glob(os.path.join('static', '*.png')):
+        for filename in glob.glob(os.path.join(temp_file_dir, '*.png')):
             os.remove(filename)
     # Use time since Jan 1, 1970 in filename in order make
     # a unique filename that the browser has not chached
-    plotfile = os.path.join('static', str(time.time()) + '.png')
+    plotfile = os.path.join(temp_file_dir, str(time.time()) + '.png')
     plt.savefig(plotfile)
     return plotfile
 
